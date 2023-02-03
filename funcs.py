abe764b6f50e75e426907aa0b6f57df12cba5474
GoodOleNothing <alexgog77777@gmail.com>
2023-02-03 20:12:11 +0500

    make sum

--- funcs.py
+++ funcs.py
@@ -1,7 +1,10 @@
-def sum_it():
-    a = 23
-    b = 10
+a = 23
+b = 10
+def sum_it(a,b):
     print(f'{a+b}')
 
+def substr_it(a,b):
+    print(f'{a-b}')
 
-sum_it()
+sum_it(a,b)
+substr_it(a,b)
---
+++ main.py
@@ -0,0 +1,3 @@
+from funcs import sum_it
+
+sum_it()
